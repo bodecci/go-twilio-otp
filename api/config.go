3dfaf834f46dfdf9c48e662c747effb66cc678c0
Bode <bode.falade@outlook.com>
2024-09-24 22:51:57 -0500

    added comments.

--- api/config.go
+++ api/config.go
@@ -9,9 +9,11 @@ import (
 
 func envACCOUNTSID() string {
 	println(godotenv.Unmarshal(".env"))
+	// Load the .env file in the current directory
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env files %v", err)
 	}
+	//use os.Getenv to retrieve an environment variable
 	return os.Getenv("TWILIO_ACCOUNT_SID")
 }
