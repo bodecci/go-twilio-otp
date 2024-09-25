97ed5cf15e71e005dae227d447a7bbe2293b28f9
Bode <bode.falade@outlook.com>
2024-09-24 22:40:33 -0500

    update envAccount func and go mod.

--- api/config.go
+++ api/config.go
@@ -3,9 +3,15 @@ package api
 import (
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-
-func envACCOUNTSID string {
-	
+func envACCOUNTSID() string {
+	println(godotenv.Unmarshal(".env"))
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env files %v", err)
+	}
+	return os.Getenv("TWILIO_ACCOUNT_SID")
 }
